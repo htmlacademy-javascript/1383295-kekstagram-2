6f5798cc1c401fcd5be124326df8c7a9a3b31a47
Rakhim <pharmamystery19@gmail.com>
2025-06-21 21:51:22 +0700

    Пытаюсь 11.14. Надо подкачаться

--- js/load-image.js
+++ js/load-image.js
@@ -1,7 +1,7 @@
 import '../vendor/pristine/pristine.min.js';
 import { modalMenu } from './modal.js';
-import { errorText, isHashtagsValid } from './validation/check-hashtag-validity.js';
-import { errorMessage, isDescriptionValid } from './validation/check-description-validity.js';
+import {errorText, isHashtagsValid, isDescriptionValid, errorMessageDescription} from './validation.js';
+
 import { sendData } from './api.js';
 import { showAlert } from './utility.js';
 import { appendNotification, closeNotification } from './notification-module.js';
@@ -9,7 +9,6 @@ import { appendNotification, closeNotification } from './notification-module.js'
 const formElement = document.querySelector('.img-upload__form');
 const hashtagsElement = document.querySelector('.text__hashtags');
 const descriptionElement = document.querySelector('.text__description');
-const formSubmit = document.querySelector('.img-upload__submit');
 
 const templateSuccess = document.querySelector('#success').content.querySelector('.success');
 const templateError = document.querySelector('#error').content.querySelector('.error');
@@ -24,29 +23,22 @@ const loadImage = () => {
   });
 
   pristine.addValidator(hashtagsElement, isHashtagsValid, errorText, 2, false);
-  pristine.addValidator(descriptionElement, isDescriptionValid, errorMessage, 2, false);
+  pristine.addValidator(descriptionElement, isDescriptionValid, errorMessageDescription, 2, false);
 
-  const sendFormData = async (x) => {
-    const isValid = pristine.validate();
-
-    if (isValid) {
-      try {
-        await sendData(new FormData(formElement));
-        appendNotification(templateSuccess, () => console.log('closeEditingForm'));
-      } catch (error) {
-        appendNotification(templateError);
-      } finally {
-        console.log('enableButton');
-      }
-    }
-  };
-
-  const formSubmitHandler = (evt) => {
+  formElement.addEventListener('submit', (evt) => {
     evt.preventDefault();
-    sendFormData(evt.target);
-  };
 
-  formSubmit.addEventListener('submit', formSubmitHandler);
+    console.log(pristine);
+
+    if (pristine.validate()) {
+      console.log('все ок');
+    } else {
+      console.log('есть вопросики');
+    }
+  });
+
+
 };
 
 export {loadImage};
+
