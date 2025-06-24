f2732d460c32a26b16578384dcbf68b58e9bcdac
Rakhim <pharmamystery19@gmail.com>
2025-06-25 00:35:54 +0700

    Остановка перед измениями

--- js/load-image.js
+++ js/load-image.js
@@ -16,12 +16,54 @@ const effectsList = formElement.querySelector('.effects__list');
 const hashtagsElement = formElement.querySelector('.text__hashtags');
 const descriptionElement = formElement.querySelector('.text__description');
 
-const imgUploadInput = document.querySelector('.img-upload__input');
-const imgOverlay = document.querySelector('.img-upload__overlay');
-const imgUploadCancel = document.querySelector('.img-upload__cancel');
+const imgUploadInput = formElement.querySelector('.img-upload__input');
+const imgOverlay = formElement.querySelector('.img-upload__overlay');
+const imgUploadCancel = formElement.querySelector('.img-upload__cancel');
 
 const templateSuccess = document.querySelector('#success').content.querySelector('.success');
 const templateError = document.querySelector('#error').content.querySelector('.error');
+
+const imgFiters = document.querySelector('.img-filters');
+const imgFiltersForm = document.querySelector('.img-filters__form');
+const imgFilterButtons = imgFiltersForm.children;
+const pictures = document.getElementsByClassName('picture');
+// const popularFilterButton = document.querySelector('#filter-discussed');
+
+const selectFilterButton = (evt) => {
+  let target = evt.target;
+  //Выбырает фильтр
+  for (let i = 0; i < imgFilterButtons.length; i++) {
+    imgFilterButtons[i].classList.remove('img-filters__button--active');
+  }
+  target.classList.add('img-filters__button--active');
+  if (target.getAttribute('id') === 'filter-default') {
+
+  }
+
+  console.log(evt.target);
+  console.log('selectFilterButton');
+};
+
+const deletePictures = () => {
+  //Удаляет фотки
+  for (let i = pictures.length - 1; i >= 0; i--) {
+    pictures[i].parentNode.removeChild(pictures[i]);
+  }
+  console.log('deletePictures');
+};
+
+const setRangePhotos = (cb) => {
+  imgFiltersForm.addEventListener('click', (evt) => {
+    deletePictures();
+
+    selectFilterButton(evt);
+    cb();
+  });
+};
+
+const showImgFiters = () => {
+  imgFiters.classList.remove('img-filters--inactive');
+};
 
 //Закрывает и Открывает модальное окно, сброс настроек
 const closeOpenModal = () => {
@@ -81,5 +123,5 @@ effectsList.addEventListener('change', onEffectRadioBtnClick);
 pristine.addValidator(hashtagsElement, isHashtagsValid, errorText, 2, false);
 pristine.addValidator(descriptionElement, isDescriptionValid, errorMessageDescription, 2, false);
 
-export {setUserFormSubmit};
+export {setUserFormSubmit, showImgFiters, setRangePhotos};
 
--- js/main.js
+++ js/main.js
@@ -1,12 +1,34 @@
 import {renderPhotos} from './render.js';
-import {setUserFormSubmit} from './load-image.js';
+import {setUserFormSubmit, showImgFiters, setRangePhotos} from './load-image.js';
 import {getData} from './api.js';
 import {showAlert} from './utility.js';
 
+// const income = [
+//   {
+//     id: 0,
+//     likes: 25
+//   },
+//   {
+//     id: 1,
+//     likes: 17
+//   },
+//   {
+//     id: 2,
+//     likes: 32
+//   }
+// ];
+// console.log(income);
+
+
 getData()
   .then((photos) => {
+    console.log(photos);
     renderPhotos(photos);
+
+    const popular = photos.sort((min, max) => max.likes - min.likes);
+    setRangePhotos(() => renderPhotos(popular));
   })
+  .then(showImgFiters())
   .catch(
     (err) => {
       showAlert(err.message);
